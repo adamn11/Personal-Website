250f94f20d67be6dadf65974eec3fca6642d97b7
Adam <adamnguyen73@gmail.com>
2019-01-21 21:52:26 -0600

    Created projects page

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,10 @@ def home():
 def about():
     return render_template('about.html')
 
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

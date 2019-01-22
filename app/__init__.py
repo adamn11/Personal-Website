9c63211b47a82eced1ddd88e4d1a0cd680d2d944
Adam <adamnguyen73@gmail.com>
2019-01-21 21:53:17 -0600

    Added contact page

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,10 @@ def about():
 def projects():
     return render_template('projects.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

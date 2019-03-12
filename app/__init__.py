53240e563ac095c392fbfefc21205558572a5804
Adam <adamnguyen73@gmail.com>
2019-03-11 21:29:43 -0500

    Moved routes from init to routes.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/bio')
-def bio():
-    return render_template('bio.html')
-
-@app.route('/resume')
-def resume():
-    return render_template('resume.html')
-
-@app.route('/projects')
-def projects():
-    return render_template('projects.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
-
+from app import routes
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from app import app
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/bio')
+def bio():
+    return render_template('bio.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
